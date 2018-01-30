9968bead66f68f9a6aa09a10494b2820e449f2cb
Aaron Duran <amduran1999@gmail.com>
2018-01-29 17:18:46 -0700

    added testing values

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from MotorControl import MotorControl
-
+import time
 
 mc = MotorControl()
 
@@ -7,9 +7,27 @@ right_address = 128
 left_address = 129
 front_address = 130
 
-controller_addresses = mc.list_controllers()
-if not (controller_addresses.__contains__(right_address) and controller_addresses.__contains__(left_address) and controller_addresses.__contains__(front_address)):
-    exit()
 
+controller_addresses = mc.list_controllers()
+print(controller_addresses)
+
+#if not (controller_addresses.__contains__(right_address) and controller_addresses.__contains__(left_address) and controller_addresses.__contains__(front_address)):
+    #exit()
+
+time.sleep(5)
+
+mc.drive_both(right_address, 1)
+mc.drive_both(left_address, 1)
+#mc.drive_M2(front_address, 1)
+time.sleep(5)
+
+mc.drive_both(right_address, -1)
+mc.drive_both(left_address, -1)
+#mc.drive_M2(front_address, -1)
+time.sleep(5)
+
+mc.drive_both(right_address, 0)
+mc.drive_both(left_address, 0)
+#mc.drive_M2(front_address, 0)
 
 # code goes here
